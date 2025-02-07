611bf889d174250a0068bb55b001b8381f18f707
Lilian Bischung <lilian.bischung@gmail.com>
2025-02-07 10:54:51 +0100

    Ajout de l'animation de soulignement sur les liens du footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,9 +4,9 @@ export default function Footer() {
     return (
         <footer className="w-full h-10 bg-background flex flex-col items-center justify-center">
             <p className="text-sm text-center flex gap-2">
-                <Link href="/contact">Contactez-moi</Link>
-                <Link href="https://github.com/Lirobi">Github</Link>
-                <Link href="https://www.linkedin.com/in/lilianbischung">Linkedin</Link>
+                <Link className="underline-animation" href="/contact">Contactez-moi</Link>
+                <Link className="underline-animation" href="https://github.com/Lirobi">Github</Link>
+                <Link className="underline-animation" href="https://www.linkedin.com/in/lilianbischung">Linkedin</Link>
             </p>
             <p>© 2025 Lilian Bischung</p>
             <p className="text-sm text-center">

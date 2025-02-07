7a66237abf2c9f4d2e7ce9eedc9f0fdc906de776
Lilian Bischung <lilian.bischung@gmail.com>
2025-02-07 10:29:36 +0100

    Commit final

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,131 @@
-import Image from "next/image";
+import DeveloppableSection from "./components/DeveloppableSection";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="min-h-screen w-screen flex flex-col">
+      <Header />
+      <main className="absolute top-1/3 w-full">
+        <div className="flex flex-col items-center gap-10 py-10 w-full">
+          <DeveloppableSection title="A propos de moi" content="Je suis un étudiant au BUT Informatique parcours <i>Réalisation d'applications: conception, développement, validation </i> à <u><i>l'IUT Robert Schuman d'Illkirch-Graffenstaden</i></u>. Je suis passionné par la création de sites web et le développement logiciel. Je suis actuellement à la recherche d'un stage pour mon cursus." />
+          <DeveloppableSection title="Formation" content={`
+        <div class="flex flex-col gap-4">
+            <h2 class="text-2xl font-bold">BUT Informatique - Parcours Réalisation d'applications : conception, développement, validation</h2>
+            
+            <p class="text-lg">
+                IUT Robert Schuman d'Illkirch-Graffenstaden
+            </p>
+            
+            <p class="text-xl">
+                <strong>Semestre 3 - Automne 2024</strong>
+            </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div class="flex flex-col gap-2">
+                <strong>Thèmes abordés :</strong>
+                <ul class="flex flex-col gap-3 list-disc ml-6">
+                    <li>
+                        <div class="flex flex-col">
+                            <span>Programmation système et réseaux (C)</span>
+                            <span class="text-sm">(Fork, Thread, Execv, Signaux, Serveurs TCP, UDP, HTTP, Sockets...)</span>
+                        </div>
+                    </li>
+                    
+                    <li>
+                        <div class="flex flex-col">
+                            <span>Conception logicielle (Java)</span>
+                            <span class="text-sm">(Patrons de conception : MVC, Observer...)</span>
+                        </div>
+                    </li>
+                    
+                    <li>Développement Web (PHP, Laravel)</li>
+                </ul>
+
+                <strong class="mt-4">Projets :</strong>
+                <ul class="flex flex-col gap-3 list-disc ml-6">
+                    <li>
+                        <div class="flex flex-col">
+                            <span>Projet T3 - Développement d'un jeu vidéo (C#)</span>
+                            <span class="text-sm">(Unity 3D, 14 semaines, équipe de 4)</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="flex flex-col">
+                            <span>Projet Labyrinthe - Jeu de plateau (Java)</span>
+                            <span class="text-sm">(Java Swing, MVC, équipe de 2)</span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <p class="text-xl">
+                <strong>Semestre 2 - Printemps 2024</strong>
+            </p>
+
+            <div class="flex flex-col gap-2">
+                <strong>Thèmes abordés :</strong>
+                <ul class="flex flex-col gap-3 list-disc ml-6">
+                    <li>
+                        <div class="flex flex-col">
+                            <span>Programmation orientée Objet (Java)</span>
+                            <span class="text-sm">(PlantUML, concepts POO)</span>
+                        </div>
+                    </li>
+                    
+                    <li>
+                        <div class="flex flex-col">
+                            <span>Développement d'applications Windows (C#, SQL)</span>
+                            <span class="text-sm">(Utilisation de .NET, Windows Forms et SQLite...)</span>
+                        </div>
+                    </li>
+                </ul>
+
+                <strong class="mt-4">Projets :</strong>
+                <ul class="flex flex-col gap-3 list-disc ml-6">
+                    <li>
+                        <div class="flex flex-col">
+                            <span>Jeu Pokemon - Jeu de cartes (Java)</span>
+                            <span class="text-sm">(Java, équipe de 2)</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="flex flex-col">
+                            <span>Application Windows - Développement d'un logiciel de gestion (C#, .NET)</span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <p class="text-xl">
+                <strong>Semestre 1 - Automne 2023</strong>
+            </p>
+            <div class="flex flex-col gap-2">
+                <strong>Thèmes abordés :</strong>
+                <ul class="flex flex-col gap-3 list-disc ml-6">
+                    <li>
+                        <div class="flex flex-col">
+                            <span>Initiation à la programmation (C#)</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="flex flex-col">
+                            <span>Initiation au développement web (HTML, CSS)</span>
+                        </div>
+                    </li>
+                </ul>
+
+                <strong class="mt-4">Projets :</strong>
+                <ul class="flex flex-col gap-3 list-disc ml-6">
+                    <li>
+                        <div class="flex flex-col">
+                            <span>Jeu snake en Assembleur</span>
+                            <span class="text-sm">(Equipe de 2)</span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        `} />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }

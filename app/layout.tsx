9996cc6611362462aa9e11a27a09af5d53d8ed89
Lilian Bischung <lilian.bischung@gmail.com>
2025-02-07 12:28:13 +0100

    Ajout des analytics vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import { Inter, Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 
@@ -40,6 +41,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }

9495ba1489d1d7dd2a4f4f166a3ce7b4114c77f4
Lilian Bischung <lilian.bischung@gmail.com>
2025-02-09 23:32:07 +0100

    Changement de l'animation de chargement

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
                 <div className="flex flex-col items-center gap-10 py-10 w-full">
                     {isLoading ? (
                         <div className="flex justify-center items-center h-full">
-                            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900 dark:border-white"></div>
+                            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 max-md:border-foreground border-white "></div>
                         </div>
                     ) : (
                         rubriques.map((rubrique: any) => (
